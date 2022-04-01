de2b42d74d5c99c3f10f25844b1c28c6f02a601e
ic2000 <icengiz@softwareinstitute.com>
2022-04-01 12:11:46 +0100

    updated gitignore

--- bob/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/msys64/home/ic2000/exercism/bob
-# Build directory: C:/msys64/home/ic2000/exercism/bob/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- robot_simulator/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/msys64/home/ic2000/exercism/robot_simulator
-# Build directory: C:/msys64/home/ic2000/exercism/robot_simulator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
